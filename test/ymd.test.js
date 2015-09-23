b5e0f5920c539d60475f804ea8a58ef4b65e80ce
JP Richardson <jprichardson@gmail.com>
2015-09-23 11:21:50 -0500

    test: moved to ava

--- test/ymd.test.js
+++ test/ymd.test.js
@@ -1,10 +1,11 @@
-var assert = require('assert')
-var ymd = require('../lib/ymd')
+var test = require('ava')
+var ymd = require('../')
 
-describe('+ ymd', function() {
-  it('should return the UTC date string', function() {
-    assert.strictEqual(ymd.utc(new Date('2013-03-05')), '2013-03-05') //JS parses this input to Date() as utc, midnight
-  })
+test('ymd() should return date string', function (t) {
+  t.is(ymd(new Date('2013-03-05 4:43 PM')), '2013-03-05', 'ymd() returns a date string')
+  t.is(ymd.utc(new Date('2013-03-05')), '2013-03-05', 'ymd.utc() returns a date string')
+  t.end()
+})
 
   it('should return the date string', function() {
     assert.strictEqual(ymd(new Date('2013-03-05 4:43 PM')), '2013-03-05')
