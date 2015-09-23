942c3146ece1d4455ef635cc45275d8008b6ef46
JP Richardson <jprichardson@gmail.com>
2015-09-23 10:56:13 -0500

    test: switch from terst to assert

--- test/ymd.test.js
+++ test/ymd.test.js
@@ -1,12 +1,12 @@
-var testutil = require('testutil')
-  , ymd = require('../lib/ymd')
+var assert = require('assert')
+var ymd = require('../lib/ymd')
 
 describe('+ ymd', function() {
   it('should return the UTC date string', function() {
-    EQ (ymd.utc(new Date('2013-03-05')), '2013-03-05') //JS parses this input to Date() as utc, midnight
+    assert.strictEqual(ymd.utc(new Date('2013-03-05')), '2013-03-05') //JS parses this input to Date() as utc, midnight
   })
 
   it('should return the date string', function() {
-    EQ (ymd(new Date('2013-03-05 4:43 PM')), '2013-03-05')
+    assert.strictEqual(ymd(new Date('2013-03-05 4:43 PM')), '2013-03-05')
   })
 })
