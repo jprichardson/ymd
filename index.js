f906f1d3b7b2fc8c1c06f331dd11700d803abb37
JP Richardson <jprichardson@gmail.com>
2015-09-23 12:14:38 -0500

    ymd: returns object instead of string

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function ymd (date) {
   date = date || new Date()
-  return date.getFullYear() + '-' + ('0' + (1 + date.getMonth())).slice(-2) + '-' + ('0' + date.getDate()).slice(-2)
+  return {
+    year: '' + date.getFullYear().toString(),
+    month: ('0' + (1 + date.getMonth())).slice(-2),
+    day: ('0' + date.getDate()).slice(-2),
+    toString: function () {
+      return this.year + '-' + this.month + '-' + this.day
+    }
+  }
 }
 
 function utc (date) {
   date = date || new Date()
-  return date.getUTCFullYear() + '-' + ('0' + (1 + date.getUTCMonth())).slice(-2) + '-' + ('0' + date.getUTCDate()).slice(-2)
+  return {
+    year: date.getUTCFullYear().toString(),
+    month: ('0' + (1 + date.getUTCMonth())).slice(-2),
+    day: ('0' + date.getUTCDate()).slice(-2),
+    toString: function () {
+      return this.year + '-' + this.month + '-' + this.day
+    }
+  }
 }
 
 ymd.utc = utc
--- test/ymd.test.js
+++ test/ymd.test.js
@@ -1,14 +1,25 @@
 var test = require('ava')
 var ymd = require('../')
 
-test('ymd() should return date string', function (t) {
-  t.is(ymd(new Date('2013-03-05 4:43 PM')), '2013-03-05', 'ymd() returns a date string')
-  t.is(ymd.utc(new Date('2013-03-05')), '2013-03-05', 'ymd.utc() returns a date string')
+test('ymd().toString() should return date string', function (t) {
+  t.is(ymd(new Date('2013-03-05 4:43 PM')).toString(), '2013-03-05')
+  t.is(ymd.utc(new Date('2013-03-05')).toString(), '2013-03-05')
   t.end()
 })
 
 test('ymd() does not require a date, defaults to current date', function (t) {
-  t.is(typeof ymd(), 'string', 'defaults to current date')
-  t.is(typeof ymd.utc(), 'string', 'defaults to current date')
+  t.is(typeof ymd(), 'object')
+  t.is(typeof ymd.utc(), 'object')
+  t.end()
+})
+
+test('ymd() should return year, month, and day', function (t) {
+  t.is(ymd(new Date('2013-03-05 4:43 PM')).year, '2013')
+  t.is(ymd(new Date('2013-03-05 4:43 PM')).month, '03')
+  t.is(ymd(new Date('2013-03-05 4:43 PM')).day, '05')
+
+  t.is(ymd.utc(new Date('2013-03-05 4:43 PM')).year, '2013')
+  t.is(ymd.utc(new Date('2013-03-05 4:43 PM')).month, '03')
+  t.is(ymd.utc(new Date('2013-03-05 4:43 PM')).day, '05')
   t.end()
 })
